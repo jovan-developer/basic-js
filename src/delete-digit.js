02676f2db1b23716fab44b2613e53a168e10e695
Jovan <jovan226@mail.ru>
2022-03-24 02:31:59 +0800

    feat: delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let numbers = String(n).split("").map(Number);
+
+  const findMin = (n1, n2, n3) => {
+    if (n1 > n2) {
+      return n2 > n3 ? [n1, n2] : [n1, n3];
+    } else {
+      return [n2, n3]
+    }
+  };
+
+  if (numbers.length <= 2) {
+    let min = Math.min(...numbers);
+    numbers.splice(numbers.indexOf(min), 1);
+  } else if (numbers.length === 3) {
+    numbers = findMin(numbers[0], numbers[1], numbers[2]);
+  } else {
+    let min = findMin(
+      numbers[numbers.length - 3],
+      numbers[numbers.length - 2],
+      numbers[numbers.length - 1]
+    );
+    numbers.splice(-1, 1);
+    numbers[numbers.length - 2] = min[0];
+    numbers[numbers.length - 1] = min[1];
+  }
+
+  return Number(numbers.join(""));
 }
 
+deleteDigit(449);
+deleteDigit(441);
+deleteDigit(1001);
+deleteDigit(152);
+
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
