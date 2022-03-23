97a513626d10e4f0fa7f11244c071f75efbaf5b6
Jovan <jovan226@mail.ru>
2022-03-24 04:04:06 +0800

    feat: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let arr = str.split("");
+  let result = [];
+  let counter = 1;
+  let idx = 0;
+  let res = ''
+
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+
+    if (i === 0) {
+      result[idx] = [el, counter];
+    } else if (arr[i] === arr[i - 1]) {
+      counter++;
+      result[idx] = [el, counter];
+    } else {
+      counter = 1;
+      idx++;
+      result[idx] = [el, counter];
+    }
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    const el = result[i];
+
+    res += el[1] > 1 ? `${el[1]}${el[0]}` : `${el[0]}`
+  }
+
+
+  return res;
 }
 
+encodeLine("aabbbcaaa");
+
 module.exports = {
-  encodeLine
+  encodeLine,
 };
